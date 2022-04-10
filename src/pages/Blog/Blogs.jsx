df76ba44c2d04747e246ed05cdd6e615d2d0cd6c
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2022-04-10 21:22:52 +0800

    blogs initial update

--- src/pages/Blog/Blogs.jsx
+++ src/pages/Blog/Blogs.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import ForestFire from './forestFire.jpg'
+import Hero from '../../components/Hero/Hero'
+import BlogListItem from '../../components/BlogListItem/BlogListItem'
+import { blogPosts } from '../blogPosts'
+import './Blog.css'
 
 const Blogs = () => {
   return (
-    <div>Blogs</div>
+    <div className='blog-page'>
+      {/* Hero Section */}
+      <Hero customY="-120px" imgSrc={ForestFire} header="Blogs" />
+
+      {/* Blog Lists */}
+      <div className="blog-list">
+        {blogPosts.map(blogItem => 
+          <BlogListItem item={blogItem} />
+        )}
+      </div>
+    </div>
   )
 }
 
