fc1adfc446d0489ecfcc79a5f896a291a9e9302d
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2022-04-15 18:21:03 +0800

    nested blogs routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
 import './assets/fonts/fonts.css'
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route, useParams } from 'react-router-dom';
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
 import Blogs from './pages/Blog/Blogs'
 import Footer from './components/Footer/Footer'
 import ScrollToTop from './components/ScrollToTop';
+import BlogArticle from './pages/BlogArticle/BlogArticle';
+import BlogEditor from './pages/BlogEditor/BlogEditor';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <ScrollToTop />
-        <Switch>
-          {/* Website Routes */}
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route exact path='/about'>
-            <About />
-          </Route>
-          <Route path='/blogs'>
+      <ScrollToTop />
+      <Switch>
+        {/* Website Routes */}
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/about'>
+          <About />
+        </Route>
+        <Route path='/blogs'>
+          <Route exact path='/blogs'>
             <Blogs />
           </Route>
+          <Route exact path='/blogs/:blogId'>
+            <BlogArticle />
+          </Route>
+          <Route exact path='/blogs/editor/:blogId'>
+            <BlogEditor />
+          </Route>
+        </Route>
 
-        </Switch>
+      </Switch>
 
-        <Footer />
-      </Router>
+      <Footer />
     </div>
   );
 }
