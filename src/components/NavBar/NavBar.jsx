3f34adf89e155a7ae394e0c9b3a2a8743a3cbf10
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2022-04-29 21:32:52 +0800

    navbar styling updates

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,30 +8,30 @@ const NavBar = ({ animation, className, customVariant, initial, currentUser }) =
   // console.log('navbar', auth)
   return (
     <>
-    <motion.div className={`navbar ${className}`}
-                animate={animation}
-                initial={initial ? initial : 'hidden'}
-                variants={customVariant ? customVariant : AnimationVariants.Navbar}
-                >
-      {/* Logo Container */}
-      <div className="site-logo">
-        Kalikasan
-      </div>
-
-      {/* nav container */}
-      <div className="nav-links">
-        <Link exact to='/' className='nav-link' activeClassName='active-nav'>Home</Link>
-        <Link to='/about' className='nav-link' activeClassName='active-nav'>About</Link>
-        {!currentUser ? 
-          <>
-            <Link exact to='/signup' className='nav-link' activeClassName='active-nav'>Sign Up</Link>
-            <Link exact to='/login' className='nav-link' activeClassName='active-nav'>Login</Link>
-          </>
-           :
-          <a onClick={onSignOut} className="nav-link">Sign Out</a>
-        }
+      <motion.div className={`navbar ${className}`}
+                  animate={animation}
+                  initial={initial ? initial : 'hidden'}
+                  variants={customVariant ? customVariant : AnimationVariants.Navbar}
+                  >
+        {/* Logo Container */}
+        <div className="site-logo">
+          Kalikasan
         </div>
-    </motion.div>
+
+        {/* nav container */}
+        <div className="nav-links">
+          <Link exact to='/' className='nav-link' activeClassName='active-nav'>Home</Link>
+          <Link to='/about' className='nav-link' activeClassName='active-nav'>About</Link>
+          {!currentUser ? 
+            <>
+              <Link exact to='/signup' className='nav-link' activeClassName='active-nav'>Sign Up</Link>
+              <Link exact to='/login' className='nav-link' activeClassName='active-nav'>Login</Link>
+            </>
+            :
+            <a onClick={onSignOut} className="nav-link">Sign Out</a>
+          }
+          </div>
+      </motion.div>
     </>
   )
 }
