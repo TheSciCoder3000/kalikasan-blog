02f863115aafd98a0d2082eb03649a781a97ad5e
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2022-04-29 21:32:04 +0800

    use window dim hook

---
+++ src/components/useWindowDim.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react'
+
+const useWindowDim = () => {
+    const [width, setWidth] = useState(window.innerWidth)
+
+    useEffect(() => {
+        function handleResize () {
+            setWidth(window.innerWidth)
+        }
+        window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
+    return width
+}
+
+export default useWindowDim
