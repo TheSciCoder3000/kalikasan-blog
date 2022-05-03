882967b3faf9b294e0f067a90360675a7cfd2e30
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2022-05-03 23:12:12 +0800

    svg import and component update

---
+++ src/pages/WebApp/iconComp/index.js
@@ -0,0 +1,4 @@
+export { default as CalendarSvg } from "./Calendar";
+export { default as DashboardSvg } from "./Dashboard";
+export { default as SchoolSvg } from "./School";
+export { default as SettingsSvg } from "./Settings";
