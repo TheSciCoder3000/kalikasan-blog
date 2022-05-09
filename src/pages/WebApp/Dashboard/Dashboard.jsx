6d612ee0a30d4613a7f2b3c1404b4c1b5a6e84b9
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2022-05-10 01:16:38 +0800

    dashboard banner update

--- src/pages/WebApp/Dashboard/Dashboard.jsx
+++ src/pages/WebApp/Dashboard/Dashboard.jsx
@@ -1,23 +1,52 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import './Dashboard.css'
+import Env from '../icons/env.svg'
 
 const Dashboard = () => {
   return (
     <div className='dashboard'>
       <div className="greeting-cont banner-cont">
-        <div className="greeting-toggler-cont"></div>
+        <FirstGreeting />
+
+        {/* slider toggler */}
+        <div className="greeting-toggler-cont">
+          <div className="toggler greeting-toggle-1 active"></div>
+          <div className="toggler greeting-toggle-2"></div>
+          <div className="toggler greeting-toggle-3"></div>
+        </div>
+      </div>
+      <div className="overall-progress-cont progress-cont banner-cont">
 
       </div>
-      <div className="overall-progress-cont banner-cont">
-
-      </div>
-      <div className="latest-task-progress-cont banner-cont">
+      <div className="latest-task-progress-cont progress-cont banner-cont">
 
       </div>
       <div className="task-list-cont banner-cont">
-
+        
       </div>
     </div>
+  )
+}
+
+const FirstGreeting = () => {
+  const variants = {
+    hover: {
+      backgroundColor: '#251308',
+      color: 'white',
+      transition: { duration: 0.11 }
+    }
+  }
+  return (
+    <motion.div className="banner banner-1">
+      <h1 className="banner-text">Welcome to <br /> <span className='logo-text'>Kalikasan</span></h1>
+      <motion.button className="start-journey"
+                     variants={variants}
+                     whileHover='hover'>
+        Start the Journey
+      </motion.button>
+      <img src={Env} alt="" className="banner-img" />
+    </motion.div>
   )
 }
 
