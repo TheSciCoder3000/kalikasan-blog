b8c51a83f5401f61d0c83e1fc3bbbdc37de8ad37
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2022-05-16 00:11:43 +0800

    participant html and css

--- src/pages/WebApp/Admin/Participant/Participant.jsx
+++ src/pages/WebApp/Admin/Participant/Participant.jsx
@@ -12,7 +12,32 @@ const Participant = () => {
   }, [])
   return (
     <div className='participant-cont'>
-      Participant
+      <div className="table-cont">
+        <div className="table-cont-header">
+          <h1 className="cont-header">Participants</h1>
+        </div>
+        <div className="table-content">
+          <div className="table-actions">
+
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th className='header-checkbox-cont'><input type="checkbox" /></th>
+                <th>Participants</th>
+                <th>Task 1</th>
+                <th>Task 2</th>
+                <th>Task 3</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   )
 }
