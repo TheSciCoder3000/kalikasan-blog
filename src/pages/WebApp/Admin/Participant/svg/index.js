6a2c5a39fb0c99bbdacaa25f9544bea46bbf363c
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2022-05-16 21:06:10 +0800

    participant page icon component

---
+++ src/pages/WebApp/Admin/Participant/svg/index.js
@@ -0,0 +1,5 @@
+export { default as Next } from "./Next";
+export { default as Previous } from "./Previous";
+export { default as SortDown } from "./SortDown";
+export { default as SortUp } from "./SortUp";
+export { default as Sort } from "./Sort";
