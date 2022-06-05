a48e8de99e7967c41bcd38e89f7b8b000524104b
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2022-06-05 20:50:04 +0800

    activity 2 responsive design and content

--- src/pages/WebApp/Lesson/LessonPages/PlantTrees/PlantTrees.jsx
+++ src/pages/WebApp/Lesson/LessonPages/PlantTrees/PlantTrees.jsx
@@ -32,6 +32,22 @@ const PlantTrees = ({ lessonId }) => {
 
   return (
     <div className='plant-trees-cont'>
+      <div className="lesson-cont">
+        <h1>Activity 2: Planting Trees</h1>
+        <p>
+          Good day participants! This week we'll be having another activity. 
+          This activity focuses on planting. Don't worry we wont be asking for 
+          too much of your time but before all that we'll provide a basic insight 
+          on how this relates to the global warming and climate change crisis.
+        </p>
+        <h2>Now It's Your Turn</h2>
+        <p>
+          Upload an image of yourself planting. 
+          May it be a tree or any specie of plant. 
+          Feel free to upload them down below.
+        </p>
+        
+      </div>
       <div className="task-container">
         {(taskData?.value && taskData?.value !== '') && !editorMode ?
           <>
