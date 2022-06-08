a84e3139d017bd14e8e06496ce9fc53e5ecde530
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2022-06-08 18:03:05 +0800

    activity 2 page for admins

--- src/pages/WebApp/Admin/Activities/ActivityPages/Planting/index.jsx
+++ src/pages/WebApp/Admin/Activities/ActivityPages/Planting/index.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import './Planting.css'
 
-const Planting = () => {
+const Planting = ({ activityId }) => {
+  const participantSubmissions = useSelector(state => state.participants.data
+    .map(participant => { return {
+      name: `${participant.FirstName} ${participant.LastName}`,
+      id: participant.id,
+      taskData: participant.tasks.find(task => task.lessonId === activityId)
+    } })
+    .filter(entry => entry.taskData ? true : false)
+  )
+
   return (
-    <div>Planting</div>
+    <div className='admin-planting'>
+      <div className="planting-header">
+        <h1>Activity #2: Planting Trees</h1>
+        <p></p>
+      </div>
+      <div className="submissions-cont">
+        {participantSubmissions.map(submissionEntry => (
+          <Link key={submissionEntry.id} className="entry-cont" to={`/app/admin/participants/${submissionEntry.id}`}>
+            <img src={submissionEntry.taskData?.value} alt="" />
+            <div className="entry-details">
+              <h4 className="participant-name">{submissionEntry.name}</h4>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
   )
 }
 
