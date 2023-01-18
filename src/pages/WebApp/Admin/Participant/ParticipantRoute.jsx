aa04b448a8af9d2ddf873bfb31dddbfbf2bd3ca4
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2023-01-18 16:54:50 +0800

    comment feature

--- src/pages/WebApp/Admin/Participant/ParticipantRoute.jsx
+++ src/pages/WebApp/Admin/Participant/ParticipantRoute.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
-import ParticipantDetail from './Detail/ParticipantDetail'
-import Participant from './Participant'
+import React from "react";
+import { Route } from "react-router-dom";
+import ParticipantDetail from "./Detail/ParticipantDetail";
+import Participant from "./Participant";
 
 const ParticipantRoute = () => {
   return (
     <>
-        <Route exact path='/app/admin/participants'>
-            <Participant />
-        </Route>
-        <Route path='/app/admin/participants/:participantId'>
-            <ParticipantDetail />
-        </Route>
+      <Route exact path="/app/admin/participants">
+        <Participant />
+      </Route>
+      <Route path="/app/admin/participants/:participantId">
+        <ParticipantDetail />
+      </Route>
     </>
-  )
-}
+  );
+};
 
-export default ParticipantRoute
+// NEW COMMENT
+
+export default ParticipantRoute;
